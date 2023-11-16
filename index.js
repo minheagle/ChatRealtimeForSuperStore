6908fa197a04e7bbaac8936ac3f1af5bf9fa4187
MinhEagle <hoangnhatminh23081998@gmail.com>
2023-11-16 14:39:10 +0700

    add origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:3000",
+    origin: ["http://127.0.0.1:3000", "https://super-store-hmh.vercel.app"],
     exposedHeaders: ["set-cookie"],
   })
 );
